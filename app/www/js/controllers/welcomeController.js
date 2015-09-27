351dbee0d19cb298fa027380ebd8b28249f9c921
jayhuang75 <jayhuang75@gmail.com>
2015-09-27 07:56:44 -0400

    Merge branch 'master' of http://github.com/TeamOrca/MasterOfCode
    # Conflicts:
    #	app/www/css/style.css
    #	app/www/index.html

--- app/www/js/controllers/welcomeController.js
+++ app/www/js/controllers/welcomeController.js
@@ -2,6 +2,15 @@
  * Created by z85603 on 15-09-26.
  */
 angular.module('supeer.controllers')
-  .controller('welcomeCtrl', function($scope) {
-      
+  .controller('welcomeCtrl', function($scope, CategoriesService, $state) {
+      $scope.services = CategoriesService.all();
+      $scope.selectedCat = $scope.services[0].name;
+
+      $scope.goToNext = function(){
+        //console.log("hello world");
+        $state.go('search-results');
+        alert($scope.selectedCat)
+
+      }
+
   });
