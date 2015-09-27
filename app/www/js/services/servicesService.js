9e0a598ef4656f1c60a0dbfbd68146124fca2053
Martin <martin.rancourt3@gmail.com>
2015-09-27 10:50:19 -0400

    Added local verseion of google maps api Added cordova-plugin-whitelist

--- app/www/js/services/servicesService.js
+++ app/www/js/services/servicesService.js
@@ -1,40 +1,40 @@
 angular.module('supeer.services')
     .factory('CategoriesService', function() {
-      // Might use a resource here that returns a JSON array
+        // Might use a resource here that returns a JSON array
 
-      // Some fake testing data
-      var categories = [
-        {
-          id : 1,
-          name : 'All',
-          image : 'http://desertsand.com.au/wp-content/uploads/2014/08/hotel.jpg'
-        },
-        {
-          id : 2,
-          name : 'HouseKeeper',
-          image : 'http://desertsand.com.au/wp-content/uploads/2014/08/hotel.jpg'
-        },
-        {
-          id : 3,
-          name : 'Plumber',
-          image : 'http://ccdci.org/wp-content/uploads/2015/03/plumber-tips.jpg'
-        }
-      ]
-
-      return {
-        all: function() {
-          return categories;
-        },
-        remove: function(chat) {
-          categories.splice(categories.indexOf(chat), 1);
-        },
-        get: function(chatId) {
-          for (var i = 0; i < categories.length; i++) {
-            if (categories[i].id === parseInt(chatId)) {
-              return categories[i];
+        // Some fake testing data
+        var categories = [
+            {
+                id : 1,
+                name : 'All',
+                image : 'http://desertsand.com.au/wp-content/uploads/2014/08/hotel.jpg'
+            },
+            {
+                id : 2,
+                name : 'HouseKeeper',
+                image : 'http://desertsand.com.au/wp-content/uploads/2014/08/hotel.jpg'
+            },
+            {
+                id : 3,
+                name : 'Plumber',
+                image : 'http://ccdci.org/wp-content/uploads/2015/03/plumber-tips.jpg'
             }
-          }
-          return null;
-        }
-      };
+        ]
+
+        return {
+            all: function() {
+                return categories;
+            },
+            remove: function(chat) {
+                categories.splice(categories.indexOf(chat), 1);
+            },
+            get: function(chatId) {
+                for (var i = 0; i < categories.length; i++) {
+                    if (categories[i].id === parseInt(chatId)) {
+                        return categories[i];
+                    }
+                }
+                return null;
+            }
+        };
     });
