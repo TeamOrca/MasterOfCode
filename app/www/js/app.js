eed566fef486a9251d248713a8f356841358db8f
Martin <martin.rancourt3@gmail.com>
2015-09-26 23:16:11 -0400

    Completed service details UI

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'supeer.services' is found in services.js
 // 'supeer.controllers' is found in controllers.js
-angular.module('supeer', ['ionic', 'supeer.controllers', 'supeer.services'])
+angular.module('supeer', ['ionic', 'supeer.controllers', 'supeer.services', 'ionic.rating'])
 
     .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
--- app/www/js/services/categoriesService.js
+++ app/www/js/services/categoriesService.js
@@ -8,32 +8,66 @@ angular.module('supeer.services')
           id : 1,
           name : 'HouseKeeper inc.',
           category : 'Heusekeeper',
-          description : 'So clean!',
+          description : "So clean! You know how to disappear. We can teach you to become truly invisible. Bruce... it's okay. Don't be afraid.Let me get this straight. You think that your client, one of the wealthiest, most powerful men in the world is secretly a vigilante who spends his nights beating criminals to a pulp with his bare hands and your plan is to blackmail this person? Good luck.",
           rate : 18,
           thumb : 'img/housekeeper_square.jpg',
-          image : 'img/housekeeper.jpg',
-          face : 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',
+          images : [
+            'img/housekeeper.jpg',
+            'img/housekeeper2.jpg',
+            'img/housekeeper3.jpg'
+          ],
+          face : 'img/cleaning_2.png',
           upvotes : 17,
           position : {
             latitude: 45.514324,
             longitude : -73.568876
-          }
+          },
+          ratings : [
+            {
+              user : 'A dude',
+              comments : "Amazing!! Now we know what they mean by 'advanced' tactical training. I'll be sure to note that in my log. Maybe we better talk out here; the observation lounge has turned into a swamp. Travel time to the nearest starbase? For an android with no feelings, he sure managed to evoke them in others. This is not about revenge. This is about justice. Mr. Worf, you do remember how to fire phasers?",
+              rating : 5
+            },
+            {
+              user : 'A Bott',
+              comments : 'Sooooo cleannn! Damage report! Yes, absolutely, I do indeed concur, wholeheartedly!',
+              rating : 4
+            }
+          ],
+          ratingAverage : 4
         },
         {
           id : 2,
           name : 'Plumber inc.',
           category : 'Plumber',
           image : 'plumber.png',
-          description : 'Magic plumbering service!',
+          description : "Magic plumbering service! My name is Merely Ducard but I speak for Ra's al Ghul... a man greatly feared by the criminal underworld. A mon who can offer you a path. Someone like you is only here by choice. You have been exploring the criminal fraternity but whatever your original intentions you have to become truly lost. The path of a man who shares his hatred of evil and wishes to serve true justice. The path of the League of Shadows.",
           rate : 15,
           thumb : 'img/plumber_square.jpg',
-          image : 'img/plumber.jpg',
-          face : 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460',
+          images : [
+            'img/plumber.jpg',
+            'img/plumber2.jpg',
+            'img/plumber3.jpg',
+          ],
+          face : 'img/logo_pre.png',
           upvotes : 24,
           position : {
             latitude : 45.512778,
             longitude : -73.558404
-          }
+          },
+          ratings : [
+            {
+              user : 'Zacky boy',
+              comments : 'Fast !!',
+              rating : 5
+            },
+            {
+              user : 'Mister x',
+              comments : 'Sooooo plubmed!',
+              rating : 4
+            }
+          ],
+          ratingAverage : 4
         }
       ]
 
